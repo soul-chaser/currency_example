4bcadad0c1b31297a065a6d63852d41ff6a486b1
sjin-hahn <sjin.hahn@samsung.com>
2023-01-11 18:10:23 +0900

    Add additional testing code for expected test goal

--- src/test/java/mainTest.java
+++ src/test/java/mainTest.java
@@ -8,5 +8,7 @@ class MainTests {
         Dollar five = new Dollar(5);
         five.times(2);
         assertEquals(10, five.amount);
+        five.times(3);
+        assertEquals(15, five.amount);
     }
 }
