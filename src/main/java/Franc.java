f4a551ddc1b1f9a79e536e3cfdfe64434d7e6322
sjin-hahn <sjin.hahn@samsung.com>
2023-01-12 16:19:47 +0900

    Copy Dollar class to Franc class for pass testFrancMultiplication test

---
+++ src/main/java/Franc.java
@@ -0,0 +1,16 @@
+public class Franc {
+    private int amount;
+
+    Franc(int amount) {
+        this.amount = amount;
+    }
+
+    Franc times(int multiplier) {
+        return new Franc(amount * multiplier);
+    }
+
+    public boolean equals(Object object) {
+        Franc dollar = (Franc) object;
+        return amount == dollar.amount;
+    }
+}
