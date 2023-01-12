2db82a53bcfcbe41a63b1085f970b1bb8da415ca
sjin-hahn <sjin.hahn@samsung.com>
2023-01-12 16:11:56 +0900

    Add testFrancMultiplication test method to check Franc currency multiplication test

--- src/test/java/mainTest.java
+++ src/test/java/mainTest.java
@@ -15,4 +15,11 @@ class MainTests {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
+
+    @Test
+    public void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
+    }
 }
