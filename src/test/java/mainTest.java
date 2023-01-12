859f1d33309b716780c2f5ed9a907cc5be480435
sjin-hahn <sjin.hahn@samsung.com>
2023-01-12 17:09:54 +0900

    Add Dollar object factory method to the Money class and change five temp variable type from the Dollar class to the Money class at the testMultiplication test method and declare Money class as a abstract and add abstract Money object return times member method to the Money abstract class

--- src/test/java/mainTest.java
+++ src/test/java/mainTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class MainTests {
     @Test
     public void testMultiplication() {
-        Dollar five = new Dollar(5);
+        Money five = Money.dollar(5);
         assertEquals(new Dollar(10), five.times(2));
         assertEquals(new Dollar(15), five.times(3));
     }
