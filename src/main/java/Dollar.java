c00b60d4a5d72de6b22aa0934d79f7c9c66b8389
sjin-hahn <sjin.hahn@samsung.com>
2023-01-12 17:40:23 +0900

    Push up each of the currency member method from the Dollar class and Franc class to the Money base class

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -1,5 +1,4 @@
 public class Dollar extends Money {
-    private String currency;
 
     Dollar(int amount) {
         this.amount = amount;
@@ -8,9 +7,5 @@ public class Dollar extends Money {
 
     Money times(int multiplier) {
         return new Dollar(amount * multiplier);
-    }
-
-    String currency() {
-        return currency;
     }
 }
--- src/main/java/Franc.java
+++ src/main/java/Franc.java
@@ -1,5 +1,4 @@
 public class Franc extends Money {
-    private String currency;
 
     Franc(int amount) {
         this.amount = amount;
@@ -8,9 +7,5 @@ public class Franc extends Money {
 
     Money times(int multiplier) {
         return new Franc(amount * multiplier);
-    }
-
-    String currency() {
-        return currency;
     }
 }
