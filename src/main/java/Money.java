3c6b9d71011713bc9e30fb3c7cea4b28ff6607c3
sjin-hahn <sjin.hahn@samsung.com>
2023-01-13 15:52:03 +0900

    Refactor Money class's member method orders

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -2,27 +2,17 @@ class Money {
     protected int amount;
     protected String currency;
 
-    Money times(int multiplier) {
-        return new Money(amount * multiplier, currency);
-    }
-
     Money(int amount, String currency) {
         this.amount = amount;
         this.currency = currency;
     }
 
-    String currency() {
-        return currency;
-    }
-
-    public boolean equals(Object object) {
-        Money money = (Money) object;
-        return amount == money.amount
-            && currency().equals(money.currency());
-    }
-
     public String toString() {
         return amount + " " + currency;
+    }
+
+    String currency() {
+        return currency;
     }
 
     static Money dollar(int amount) {
@@ -31,5 +21,15 @@ class Money {
 
     static Money franc(int amount) {
         return new Money(amount, "CHF");
+    }
+
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount
+                && currency().equals(money.currency());
+    }
+
+    Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
     }
 }
