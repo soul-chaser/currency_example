274e5b1782cbca9630aaa3be5dde951ca14feb41
sjin-hahn <sjin.hahn@samsung.com>
2023-01-13 17:27:35 +0900

    Add testReduceSum test method to check Bank class's reduce member method returns correct Sum object result value

--- src/test/java/mainTest.java
+++ src/test/java/mainTest.java
@@ -40,4 +40,12 @@ class MainTests {
         assertEquals(five, sum.augend);
         assertEquals(five, sum.addend);
     }
+
+    @Test
+    public void testReduceSum() {
+        Expression sum = new Sum(Money.dollar(3), Money.dollar(4));
+        Bank bank = new Bank();
+        Money result = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(7), result);
+    }
 }
