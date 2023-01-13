17db7aabc48f60018f86ab93ad403ffdbc5d1ddc
sjin-hahn <sjin.hahn@samsung.com>
2023-01-13 15:05:52 +0900

    Change franc member method returns Money object instead of the Franc object and also dollar member method returns Money object instead of the Dollar object and delete Dollar class because it is not used anymore

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -26,10 +26,10 @@ class Money {
     }
 
     static Money dollar(int amount) {
-        return new Dollar(amount, "USD");
+        return new Money(amount, "USD");
     }
 
     static Money franc(int amount) {
-        return new Franc(amount, "CHF");
+        return new Money(amount, "CHF");
     }
 }
