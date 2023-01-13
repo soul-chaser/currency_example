577b2c69748209ba79d56299a9d999d336011550
sjin-hahn <sjin.hahn@samsung.com>
2023-01-13 15:12:56 +0900

    Delete testDifferentClassEquality test method because testEquality test method already covers same test and remove 3rd, 4th assert code because 1st, 2nd assert already covers same test and delete Franc class because it is not used anymore

--- src/test/java/mainTest.java
+++ src/test/java/mainTest.java
@@ -14,8 +14,6 @@ class MainTests {
     public void testEquality() {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 
@@ -30,10 +28,5 @@ class MainTests {
     public void testCurrency() {
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
-    }
-
-    @Test
-    public void testDifferentClassEquality() {
-        assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
     }
 }
