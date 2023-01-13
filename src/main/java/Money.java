67e0cfbcf0ea487f87c4241dbcbad158e78327aa
sjin-hahn <sjin.hahn@samsung.com>
2023-01-13 16:58:55 +0900

    Change plus returns Sum object to fix following compile error and add Sum class's constructor and Sum class implements Expression
    > Task :test FAILED
    
    class Money cannot be cast to class Sum (Money and Sum are in unnamed module of loader 'app')
    java.lang.ClassCastException: class Money cannot be cast to class Sum (Money and Sum are in unnamed module of loader 'app')
    	at MainTests.testPlusReturnsSum(mainTest.java:39)

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -34,6 +34,6 @@ class Money implements Expression {
     }
 
     Expression plus(Money addend) {
-        return new Money(amount + addend.amount, currency);
+        return new Sum(this, addend);
     }
 }
--- src/main/java/Sum.java
+++ src/main/java/Sum.java
@@ -1,4 +1,7 @@
-public class Sum {
+public class Sum implements Expression {
     Money augend;
     Money addend;
+
+    public Sum(Money augend, Money addend) {
+    }
 }
