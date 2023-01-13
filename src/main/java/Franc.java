2c4f52cc7502a816be0b5a47a331b741313ae1a7
sjin-hahn <sjin.hahn@samsung.com>
2023-01-13 14:21:40 +0900

    Change factory method call back to constructor method call in the times member method

--- src/main/java/Franc.java
+++ src/main/java/Franc.java
@@ -5,6 +5,6 @@ public class Franc extends Money {
     }
 
     Money times(int multiplier) {
-        return Money.franc(amount * multiplier);
+        return new Franc(amount * multiplier, "CHF");
     }
 }
