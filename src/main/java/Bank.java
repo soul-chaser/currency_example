673bad149b118d633fdc7694c706a85686fb234b
sjin-hahn <sjin.hahn@samsung.com>
2023-01-19 16:52:17 +0900

    Add Pair class to store currency pair as key of the hashtable and add rates hashtable member variable stores currency change rate to the Bank class and add addRate member method configure currency change rate to the Bank class add currency change rate from CHF to USD as 2 into the testReduceMoneyDifferentCurrency JUnit test method

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,11 +1,18 @@
+import java.util.Hashtable;
+
 public class Bank {
+    private Hashtable rates = new Hashtable();
+
     Money reduce(Expression source, String to) {
         return source.reduce(this, to);
     }
 
     int rate(String from, String to) {
-        return (from.equals("CHF") && to.equals("USD"))
-                ? 2
-                : 1;
+        Integer rate = (Integer) rates.get(new Pair(from, to));
+        return rate.intValue();
+    }
+
+    void addRate(String from, String to, int rate) {
+        rates.put(new Pair(from, to), new Integer(rate));
     }
 }
--- src/test/java/mainTest.java
+++ src/test/java/mainTest.java
@@ -59,6 +59,7 @@ class MainTests {
     @Test
     public void testReduceMoneyDifferentCurrency() {
         Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
         Money result = bank.reduce(Money.franc(2), "USD");
         assertEquals(Money.dollar(1), result);
     }
