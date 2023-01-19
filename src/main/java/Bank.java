a7c400cb4f8e9af3aa51618d739e4d3f578da0f9
sjin-hahn <sjin.hahn@samsung.com>
2023-01-19 15:14:48 +0900

    Move change rate decision from Money class's reduce member method to Bank class's rate member method to give Bank class has responsibility of change rate

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,5 +1,11 @@
 public class Bank {
     Money reduce(Expression source, String to) {
-        return source.reduce(to);
+        return source.reduce(this, to);
+    }
+
+    int rate(String from, String to) {
+        return (from.equals("CHF") && to.equals("USD"))
+                ? 2
+                : 1;
     }
 }
